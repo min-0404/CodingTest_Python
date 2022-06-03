1b9c6d4e3e7de0da872c819f746e84ae8bd1eecf
Minseok Kim <minsuck040484@gmail.com>
2022-06-03 16:13:59 +0900

    20220603

---
+++ Programmers/완주하지못한선수.py
@@ -0,0 +1,21 @@
+# Hash 이용
+def solution(participant, completion):
+    
+    dict = {}
+    sum = 0;
+    
+    for i in participant:
+        dict[hash(i)] = i;
+        sum += hash(i);
+    for i in completion:
+        sum -= hash(i);
+        
+    return dict[sum];
+
+# Counter 이용 (딕셔너리와 비슷한데, key 는 이름, value 는 값)
+# import collections
+# def solution(participant, completion):
+
+    # answer = collections.Counter(participant) - collections.Counter(completion)
+    
+    # return list(answer.keys())[0];
---
+++ p.py
@@ -0,0 +1,41 @@
+# from operator import index
+
+
+# number = "1924"
+# k = 2
+
+# # lst = list(map(int, number));
+# # lst2 = sorted(lst);
+# # result = lst2[:k]
+
+# # for i in lst[:]:
+# #     if i in result:
+# #         lst.remove(i);
+
+# stack = [number[0]];
+# for i in number[1:]:
+#     while stack and stack[-1] < i and k >0:
+#         stack.pop();
+#         k -= 1;
+#     stack.append(i);
+
+# answer = "".join(stack)
+# print(answer)  
+
+# stack = [number[0]]
+# for num in number[1:]:
+#     while stack and stack[-1] < num and k > 0:
+#         stack.pop()
+#         k -= 1
+#     stack.append(num)
+        
+# return "".join(stack) if k == 0 else "".join(stack[:-k])
+
+dict = {}
+sum = 0;
+num = ["min", "seok"];
+for i in num:
+    dict[hash(i)] = i;
+    sum += hash(i);
+print(dict)
+print(sum)
