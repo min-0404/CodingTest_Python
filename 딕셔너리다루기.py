7d4de1ab34a416802b0e61f6c80ac50a454619cb
Minseok Kim <minsuck040484@gmail.com>
2022-07-13 21:49:57 +0900

    .

--- 딕셔너리다루기.py
+++ 딕셔너리다루기.py
@@ -1,3 +1,14 @@
 dict = {'a': 1, 'b': 2, 'c' : 3};
 
-for key in 
+for key in dict:
+    print(key);
+
+for key in dict.keys():
+    print(key);
+
+for value in dict.values():
+    print(value);
+
+for key, value in dict.items():
+    print(key, value);
+                  
